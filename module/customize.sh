89a9d3bd3ebf2959099b95a4b85f6fec2e057fae
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-02-09 12:24:24 +0300

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -1,5 +1,3 @@
-# Please do not remove the credit Telegram @chinacloudgroup
-
 # info
 SOC=`getprop ro.soc.model`
 MODVER=`grep_prop version $MODPATH/module.prop`
@@ -12,10 +10,16 @@ echo " - Success 🧭"
 else
 echo " "
 echo "❗ Very bad rom (risky)"
-echo " "
 fi
 echo " "
 ui_print " - Android version check..."
 [ $(getprop ro.system.build.version.sdk) -lt 29 ] && echo "! Unsupported android version detected, please upgrade." && abort
 echo " - Success"
 echo " "
+# Vulkan 1.3 no need A730+ @tryigitx
+VULKAN_MODEL=$(getprop ro.hardware.vulkan)
+SOC=$(getprop ro.soc.model)
+SOC_NUM=${SOC:2}
+if [[ $SOC_NUM -gt 8440 ]] && [[ $VULKAN_MODEL == "adreno" ]]; then
+    rm -rf $MODPATH/system/vendor/etc/
+fi
