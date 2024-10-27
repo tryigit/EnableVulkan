be637e5d5ac9abad385db7858fea22ac3ec40d01
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-10-27 13:06:16 +0300

    Update post-fs-data.sh

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -1,6 +1,6 @@
 # Credit @tryigitx Android 14 > Check
 if [[ $(getprop ro.build.version.sdk) -lt 34 ]]; then
-    resetprop debug.hwui.renderer vulkan
+    resetprop debug.hwui.renderer skiavk
 fi
 
 # Vip Features -If you do not have a VIP driver, it may also cause instability during bootup.-
